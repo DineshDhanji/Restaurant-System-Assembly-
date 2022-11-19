6ed8a0ae0e61f6e44c92be15d021e04f0c2ad641
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2022-11-19 17:12:16 +0500

    Update Loading.asm

--- Loading.asm
+++ Loading.asm
@@ -10,7 +10,7 @@ two BYTE 2												; Just a variable to divide register(s)/vaiable(s) without
 LoadingScreenBar BYTE "___",0
 LoadingScreenText01 BYTE "Have you lost weight?",0
 LoadingScreenText02 BYTE "Life was supposed to be great ......",0
-LoadingScreenText03 BYTE "Warning: Don't set yourself on fire",0
+LoadingScreenText03 BYTE " Warning: Don't set yourself on fire",0
 LoadingScreenText04 BYTE "Would you prefer chicken, steak, or tofu?",0
 LoadingScreenText05 BYTE "Don't you think that he bits are flowing slowly today ?",0
 						
