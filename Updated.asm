f5e20c531ac5deef48fd17aaf22e98db58b5755c
Dinesh Dhanji <56204876+DineshDhanji@users.noreply.github.com>
2022-11-23 13:21:54 +0500

    Update Updated.asm

--- Updated.asm
+++ Updated.asm
@@ -4,57 +4,57 @@ BUFFER_SIZE = 5000
 TotalItems = 24
 
 .data
+count BYTE 0
+row BYTE 1 
+two BYTE 2												; Just a variable to divide register(s)/vaiable(s) without disturbing other registers
+col BYTE 65
 maxX BYTE ?												; Maximun size with respect to X
 maxY BYTE ?												; Maximun size with respect to Y
 CenterX BYTE ?											; Center with respect to X
 CenterY BYTE ?											; Center with respect to Y
-two BYTE 2												; Just a variable to divide register(s)/vaiable(s) without disturbing other registers
+
+buffer BYTE BUFFER_SIZE DUP(?)
+filename BYTE "menu.txt", 0
+fileHandle HANDLE ?
+
+item1 BYTE "Chicken Biryani    ", 0
+item2 BYTE "Beef Biryani       ", 0
+item3 BYTE "Chicken Karahi     ", 0
+item4 BYTE "Mutton Karahi      ", 0
+item5 BYTE "White Karahi       ", 0
+item6 BYTE "Chicken Haleem     ", 0
+item7 BYTE "Chicken Tikka      ", 0
+item8 BYTE "Naan               ", 0
+item9 BYTE "Chapati            ", 0
+item10 BYTE "Egg Fried Rice    ", 0
+item11 BYTE "Chicken Macaroni  ", 0
+item12 BYTE "Chicken Manchurian", 0
+item13 BYTE "Chowmein          ", 0
+item14 BYTE "Zinger Burger     ", 0
+item15 BYTE "Chicken Pizza     ", 0
+item16 BYTE "Chicken Shawarma  ", 0
+item17 BYTE "French Fries      ", 0
+item18 BYTE "Coca Cola         ", 0
+item19 BYTE "Sprite            ", 0
+item20 BYTE "Pine Apple Juice  ", 0
+item21 BYTE "Banana Milkshake  ", 0
+item22 BYTE "Chocolate Cake    ", 0
+item23 BYTE "Coconut Cream Pie ", 0
+item24 BYTE "Apple Pie         ", 0
+item25 BYTE "Vanilla Icecream  ", 0
+
+items DWORD item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25
+price DWORD 100, 150, 600, 900, 1000, 100, 100, 15, 10, 150, 300, 300,  200, 100, 800, 200, 100, 100, 100, 80, 60, 1000, 600, 300, 200
+Order SWORD TotalItems DUP(-1)
+Quantity WORD TotalItems DUP(1)
+
+
 LoadingScreenBar BYTE "___",0
 LoadingScreenText01 BYTE "Have you lost weight?",0
 LoadingScreenText02 BYTE "Life was supposed to be great ......",0
 LoadingScreenText03 BYTE " Warning: Don't set yourself on fire",0
 LoadingScreenText04 BYTE "Would you prefer chicken, steak, or tofu?",0
 LoadingScreenText05 BYTE "Don't you think that the bits are flowing slowly today ?",0
-
-
-
-buffer BYTE BUFFER_SIZE DUP(?)
-filename BYTE "menu.txt", 0
-fileHandle HANDLE ?
-
-item1 BYTE "Chicken Biryani", 0
-item2 BYTE "Beef Biryani", 0
-item3 BYTE "Chicken Karahi", 0
-item4 BYTE "Mutton Karahi", 0
-item5 BYTE "White Karahi", 0
-item6 BYTE "Chicken Haleem", 0
-item7 BYTE "Chicken Tikka", 0
-item8 BYTE "Naan", 0
-item9 BYTE "Chapati", 0
-item10 BYTE "Egg Fried Rice", 0
-item11 BYTE "Chicken Macaroni", 0
-item12 BYTE "Chicken Manchurian", 0
-item13 BYTE "Chowmein", 0
-item14 BYTE "Zinger Burger", 0
-item15 BYTE "Chicken Pizza", 0
-item16 BYTE "Chicken Shawarma", 0
-item17 BYTE "French Fries", 0
-item18 BYTE "Coca Cola", 0
-item19 BYTE "Sprite", 0
-item20 BYTE "Pine Apple Juice", 0
-item21 BYTE "Banana Milkshake", 0
-item22 BYTE "Chocolate Cake", 0
-item23 BYTE "Coconut Cream Pie", 0
-item24 BYTE "Apple Pie", 0
-item25 BYTE "Vanilla Icecream", 0
-items DWORD item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21, item22, item23, item24, item25
-price DWORD 100, 150, 600, 900, 1000, 100, 100, 15, 10, 150, 300, 300,  200, 100, 800, 200, 100, 100, 100, 80, 60, 1000, 600, 300, 200
-
-row BYTE 1 
-col BYTE 65
-
-count BYTE 0
-
 str1 BYTE "Enter -1 when your order is complete", 0
 str2 BYTE "Enter Item Number to place your order: ", 0
 str3 BYTE "		Total Bill:		", 0
@@ -62,16 +62,19 @@ str4 BYTE "			BILL DETAIL", 0
 str5 BYTE "Enter quantity: ",0
 str6 BYTE "ID     ITEM     PRICE     QTY",0
 space BYTE "     ", 0
-
-Order SWORD TotalItems DUP(-1)
-Quantity WORD TotalItems DUP(1)
+IdHeading BYTE "ID",0
+ItemHeading BYTE "ITEM",0
+PriceHeading BYTE "PRICE",0
+QuantityHeading BYTE "QTY",0
+TIPHeading BYTE "TOTAL ITEM PRICE",0
+TotalHeading BYTE "TOTAL",0
 
 .code
 main PROC
 
 call PrintMenu
 call GetOrder
-call RemoveEndingLines
+;call RemoveEndingLines
 exit
 main ENDP
 
@@ -217,14 +220,129 @@ GetOrder ENDP
 GeneratingBill PROC
 	call ReadMyScreenDetails
 	call clrscr
-	call LoadingScreen
+	;call LoadingScreen
 	call clrscr
 	call PrintBill
 	ret
 GeneratingBill ENDP
 
 PrintBill PROC
+	LOCAL NewX : BYTE
+	LOCAL NewY : BYTE
+	LOCAL totalBill : DWORD
+	LOCAL tempPrice : WORD
+
+	mov totalBill, 0
+	;Seting new cursor position for bill
+	mov al, CenterX 
+	SHR al, 1
+	mov bl, CenterY
+	SHR bl, 2
+	mov NewX, al
+	mov NewY, bl
+
+
+	mov dh, NewY
+	mov dl, NewX
+	call Gotoxy
+	;mov edx, OFFSET space
+	;call WriteString
+	mov edx, OFFSET str4
+	call WriteString
 	
+	add NewY, 2
+	sub NewX, 6
+	mov dh, NewY
+	mov dl, NewX
+	call Gotoxy
+	
+	mov edx, OFFSET IdHeading
+	call WriteString
+	mov edx, OFFSET space
+	call WriteString
+	call WriteString
+
+	mov edx, OFFSET ItemHeading
+	call WriteString
+	mov edx, OFFSET space
+	call WriteString
+	call WriteString
+	call WriteString
+
+	
+	mov edx, OFFSET PriceHeading
+	call WriteString
+	mov edx, OFFSET space
+	call WriteString
+
+	
+	mov edx, OFFSET QuantityHeading
+	call WriteString
+	mov edx, OFFSET space
+	call WriteString
+	call WriteString
+
+	mov edx, OFFSET TIPHeading
+	call WriteString
+	
+	mov esi, 0
+
+	.WHILE(Order[esi * TYPE WORD] != -1) 
+		inc NewY
+		mov dh, NewY
+		mov dl, NewX
+		call Gotoxy
+		; Printing items' id and then space.
+		movzx eax, Order[esi * TYPE WORD]
+		call WriteDec
+		mov edx, OFFSET space
+		call WriteString
+
+
+		; Printing items' name and then space.
+		movzx eax, Order[esi * TYPE WORD]
+		dec eax
+		mov edx, items[eax* TYPE DWORD]
+		call WriteString
+		mov edx, OFFSET space
+		call WriteString
+		
+		
+		; Printing items' price and then space.
+		movzx eax, Order[esi * TYPE WORD]
+		dec eax
+		mov eax, price[eax* TYPE DWORD]
+		call WriteDec
+		mov tempPrice, ax
+		mov edx, OFFSET space
+		call WriteString
+		call WriteString
+
+		; Printing items' quantity and then space.
+		movzx eax, Quantity[esi * TYPE WORD]
+		call WriteDec
+		mov edx, OFFSET space
+		call WriteString
+		call WriteString
+
+		mul tempPrice
+		add totalBill, eax
+		call WriteDec
+		inc esi
+	.ENDW
+	add NewY, 2
+	add NewX, 34
+	mov dh, NewY
+	mov dl, NewX
+	call Gotoxy
+	mov edx, OFFSET TotalHeading
+	call WriteString
+	mov edx, OFFSET space
+	call WriteString
+	call WriteString
+	call WriteString
+	mov eax, totalBill
+	call WriteDec
 	ret
 PrintBill ENDP
 
